0008e09112d8d2caf8ec475b31185dfc9ed8b067
Nicole Garcia <nickygarcia220998@gmail.com>
2018-11-19 18:28:17 -0400

    prueba de conexion al servidor hydra60 de github para el desarrollo

--- app/src/main/java/com/example/rex/appandroid/Adapters/AdapterBook.java
+++ app/src/main/java/com/example/rex/appandroid/Adapters/AdapterBook.java
@@ -14,6 +14,7 @@ import com.example.rex.appandroid.R;
 import java.util.ArrayList;
 
 public class AdapterBook extends BaseAdapter{
+
     private ArrayList<Book>  listItem;
     private Context context;
 
