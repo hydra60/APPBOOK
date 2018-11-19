d5e6d390428ed0f01dba1aacf692e780bfab0197
Benjamin <bj606340@gmail.com>
2018-11-19 10:58:24 -0400

    Adicion de la vista Search_book y implemantacion de listView

--- app/src/main/java/com/example/rex/appandroid/SearchBook.java
+++ app/src/main/java/com/example/rex/appandroid/SearchBook.java
@@ -2,16 +2,12 @@ package com.example.rex.appandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchBook extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void searchBook(View view) {
+        setContentView(R.layout.activity_search_book);
     }
 }
